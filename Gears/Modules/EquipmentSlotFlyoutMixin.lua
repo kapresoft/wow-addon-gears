52f031d582c88692dcd63bd4dc61628329f2ed88
Tony Lagnada <deloyz@gmail.com>
2026-03-12 17:53:10 -0700

    finalize margins

--- Gears/Modules/EquipmentSlotFlyoutMixin.lua
+++ Gears/Modules/EquipmentSlotFlyoutMixin.lua
@@ -27,17 +27,26 @@ local p, pd, t, tf = ns:log(libName)
 --[[-----------------------------------------------------------------------------
 Module::EquipmentSlotFlyoutMixin (Methods)
 -------------------------------------------------------------------------------]]
---- @type EquipmentSlotFlyoutMixin
+--- @type EquipmentSlotFlyoutMixin | ButtonObj
 local o = Gears_EquipmentSlotFlyoutMixin
 
 function o:OnLoad()
-  BackdropTemplateMixin.OnBackdropLoaded(self)
+  self:ClearAllPoints()
+  local ofsx, ofsy = -2, 0
+  if ns:HasBlizzEquipmentManager() then ofsx = -10 end
+  self:SetPoint('LEFT', CharacterShoulderSlot, 'RIGHT', ofsx, ofsy)
+  
   self.Flyout:SetBackdropColor(0.25, 0.32, 0.50, 0.95)
   self.Flyout:SetBackdropBorderColor(1.0, 0.84, 0.0, 0.95)
   self:CreateActionButtons()
   self.Arrow:SetTexture(310765);
   self.Arrow:SetTexCoord(0.02, 0.98, 0.02, 0.48);
   self:FlyoutShow()
+  
+end
+
+function o:OnShow()
+
 end
 
 function o:CreateActionButtons()
@@ -66,11 +75,13 @@ function o:CreateActionButtons()
 end
 
 function o:FlyoutHide()
+  ns:PlaySound(SOUNDKIT.IG_MINIMAP_CLOSE)
   self.Arrow:SetRotation(math.rad(-90)) -- ▶ collapsed
   self.Flyout:Hide()
 end
 
 function o:FlyoutShow()
+  ns:PlaySound(SOUNDKIT.IG_MINIMAP_OPEN)
   self.Flyout:Show()
   self.Arrow:SetRotation(math.rad(90)) -- ◀ expanded
 end
@@ -81,8 +92,8 @@ function o:OnClick()
 end
 
 function o:OnEnter()
-  self.Arrow:SetVertexColor(1, 0.95, 0.4, 1)
-  self.Arrow:SetBlendMode("ADD")
+  self.Arrow:SetVertexColor(0.4, 0.95, 0.4, 1)
+  self.Arrow:SetBlendMode("BLEND")
 end
 
 function o:OnLeave()
